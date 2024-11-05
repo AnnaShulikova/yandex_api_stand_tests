bfe38ca7158f3aeaabb92c59341ce14d5026b5f2
Шуликова Анна Андреевна <a.shulikova@inlinegroup.ru>
2024-11-05 20:08:22 +0300

    My first commit

---
+++ sender_stand_request.py
@@ -0,0 +1,35 @@
+# Импортируем модуль configuration, который, мы создали выше - он содержит настройки подключения и путь к документации
+# from json.decoder import JSONObject
+
+import configuration
+
+import requests
+
+import data
+
+# Определяем функцию get_docs, которая не принимает параметров
+def get_docs():
+    # Выполняем GET-запрос к URL, который складывается из базового URL-адреса сервиса
+    # и пути к документации, заданных в модуле конфигурации
+    # Функция возвращает объект ответа от сервера
+    return requests.get(configuration.URL_SERVICE + configuration.DOC_PATH)
+
+def get_logs():
+    return requests.get(configuration.URL_SERVICE + configuration.LOG_MAIN_PATH, params={"count":20})
+
+def get_users_table():
+    return requests.get(configuration.URL_SERVICE + configuration.USERS_TABLE_PATH)
+
+def post_new_user(body):
+    return requests.post(configuration.URL_SERVICE + configuration.CREATE_USER_PATH,
+                         json=body,
+                         headers=data.headers)
+
+def post_products_kits(products_ids):
+    return requests.post(configuration.URL_SERVICE + configuration.PRODUCTS_KITS_PATH,
+                         json=products_ids,
+                         headers=data.headers)
+
+
+
+
